443f8cb30f593f7bccd003fa98a38355b70763c9
RubinoD1 <Rubinod1@gmail.com>
2023-12-10 22:27:23 -0500

    Progress on html with homepage and cards

---
+++ assets/js/logic.js
@@ -0,0 +1,18 @@
+const startBTN = document.getElementById("start-btn");
+const settingsBTN = document.getElementById("settings-btn");
+//delete once selection is made -- may need to adjust based on if settings is selected
+const homeBTNs = document.getElementById("home-btns");
+
+
+//btn event listeners 
+//TO-DO: Consolidate into one || case selection/ if/else to sort event input 
+startBTN.addEventListener("click", myFunction1);
+settingsBTN.addEventListener("click", myFunction1);
+
+function myFunction1 (){
+    console.log("Click");
+    //delete homeBTNs cointainer
+    
+    //uhide qoute selection cards
+
+}
