a31a2e4bcef5db8c83ef5723d9cf15497f7825b6
RubinoD1 <Rubinod1@gmail.com>
2023-12-12 01:00:06 -0500

    added wireframes for refrence and cleaned up js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,18 +1,19 @@
 const startBTN = document.getElementById("start-btn");
 const settingsBTN = document.getElementById("settings-btn");
-//delete once selection is made -- may need to adjust based on if settings is selected
 const homeBTNs = document.getElementById("home-btns");
+const cards = document.getElementById("cards");
 
 
 //btn event listeners 
 //TO-DO: Consolidate into one || case selection/ if/else to sort event input 
-startBTN.addEventListener("click", myFunction1);
-settingsBTN.addEventListener("click", myFunction1);
+startBTN.addEventListener("click", startScreen);
+settingsBTN.addEventListener("click", startScreen);
 
-function myFunction1 (){
+//start screen function to hide btns and unhide cards
+function startScreen (){
     console.log("Click");
     //delete homeBTNs cointainer
-    
+    homeBTNs.remove();
     //uhide qoute selection cards
-
+    cards.classList.remove("hidden");
 }
