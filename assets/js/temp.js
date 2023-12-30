5d084c62223cb7f9c9ed5845f09a8bef938295ac
RubinoD1 <Rubinod1@gmail.com>
2023-12-30 08:09:31 -0500

    setup temp arrays, progress on theme select and cleaned up some code

--- assets/js/temp.js
+++ assets/js/temp.js
@@ -1,15 +1,55 @@
-const startBTN = document.getElementById("start-btn");
-const settingsBTN = document.getElementById("settings-btn");
 const homeBTNs = document.getElementById("home-btns");
 const cards = document.getElementById("cards");
 
-startBTN.addEventListener("click", startScreen);
-settingsBTN.addEventListener("click", startScreen);
-
-function startScreen (){
-    console.log("Click");
+//sorts user clicks  
+document.onclick = function(event) {
+   let target = event.target.id
+   //var target = event.target.innerHTML; 
+   //event.target.id
+   //console.log(target);
+   if(target === "start-btn"){
+    console.log("start");
     //delete homeBTNs cointainer
     homeBTNs.remove();
     //uhide qoute selection cards
     cards.classList.remove("hidden");
+   } else if (target === "nietzsche" || target === "shakespeare" || target === "mystery"){
+     console.log(target, "has been clicked");
+     //open settings menu 
+     
+     let theme = arrays[target]; //set array selection
+     //console.log(theme);
+     themeSelect(theme);//pass value to function to set quotes
+   }
+};
+
+
+//function for selecting qoutes, randomizing and setting str value for starting qoute
+function themeSelect(theme){
+  console.log(theme, "is in theme select");
+  //***RE-ADD HIDDEN CLASS???***
+  cards.remove();//delete cards div 
+  //randomize array 
+     //theme = array choice
+
+  //set first qoute 
+  //call qoute function
+  
+ 
+  //set str
+  //unhide || load qoute html file
 }
+
+
+
+
+
+//arrays
+const arrays = {
+  shakespeare: ["shakespeare0", "shakespeare1", "shakespeare2","shakespeare3", "shakespeare4", "shakespeare5"],
+  nietzsche: ["nietzsche0" ,"nietzsche1", "nietzsche2", "nietzsche3", "nietzsche4", "nietzsche5"],
+  mystery: ["mystery0" ,"mystery1", "mystery2", "mystery3", "mystery4", "mystery5"]
+}
+
+
+//console.log(arrays.mystery);
