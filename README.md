7240fa8b8d9e4a431eeefdf1f605aaf8a8eb5dec
RubinoD1 <Rubinod1@gmail.com>
2024-01-26 06:29:43 -0500

    consolidated CSS repeats, cleaned up logic.js format, and updated Readme file

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# Keyboard Warrior
-![Site banner](../keyboard%20warrior/assets/images/readmeBanner.png)
+![Site banner](/assets/images/readmeBanner.png)
 
 ![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white) ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white) ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
 
@@ -12,8 +11,6 @@ Live page:
 Keyboard Warrior is a JavaScript built typing app. It cointains 12 selectable themes, each with a set of 10 randomized qoutes. The themes are mainly centered around literature, philosophy, and historical figures. 
 
 An accuracy percentage is scored per qoute with a total percentage being given at the completion of the tenth qoute.  
-
-## More Info 
 
 ## Screenshots
 ![Homepage](/assets/images/screenshots/homepage.png)
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -13,47 +13,47 @@ let correct = 0;//tracks correct inputs
 
 //button event listener for cards and replay button
 document.onclick = function(event) {
-let target = event.target.id
-
-switch(target) {
-  case "nietzsche":
-  case "shakespeare":
-  case "mystery":
-  case "sontag":
-  case "churchill":
-  case "napeleon":
-  case "beauvoir":
-  case "lawrence":
-  case "tolkein":
-  case "camus":
-  case "tolstoy":
-  case "jung":  
-   localStorage.setItem("theme", `${target}`);//save theme to local storage
-   window.location.assign("main.html");//load main.html page
-   break;
-  case "replay":
-   localStorage.clear();//clear loacal storage -- NEEDED?
-   window.location.assign("cards.html");//load card.html page
-   break;
+ let target = event.target.id
+ switch(target) {//card btns
+   case "nietzsche":
+   case "shakespeare":
+   case "mystery":
+   case "sontag":
+   case "churchill":
+   case "napeleon":
+   case "beauvoir":
+   case "lawrence":
+   case "tolkein":
+   case "camus":
+   case "tolstoy":
+   case "jung":  
+    localStorage.setItem("theme", `${target}`);//save theme to local storage
+    window.location.assign("main.html");//load main.html page
+    break;
+   case "replay"://replay btn
+    localStorage.clear();//clear loacal storage 
+    window.location.assign("cards.html");//load card.html page
+    break;
   }
 };
 
-//Keyboard input listener -- WILL NEED TO CHECK TABLET COMPATIBILITY
+//Keyboard input listener 
 document.addEventListener('keydown', (event) => {
   event.preventDefault();//prevents space key from moving screen position and accidental ctl + f presses
   
   if (position < length){ 
       inputFilter(event);
-  } else if (position >= length) {//Trigger next qoute if avaliable and array not at last #
+  } 
+  else if (position >= length) {//Trigger next qoute if avaliable and array not at last #
       arrayPosition = arrayPosition + 1;//increase arrayPosition 
       accuracyScores.push(Math.trunc(correct/position*100));//push accuracy value to total array  
       accuracyHTML.innerHTML = "Accuracy: --%";//reset accuracy percentage HTML for next qoute
     if(arrayPosition < theme.length){
-      let progress = Math.trunc(arrayPosition/theme.length*100);//increase progress bar --calculate bar completion %
+      let progress = Math.trunc(arrayPosition/theme.length*100);//increase progress bar 
       document.getElementById("progress-bar").setAttribute("value", `${progress}`);
       //Update qoutes left HTML
       document.getElementById("qoutes-left").innerHTML = "Quotes Complete: " + arrayPosition + " / " + theme.length;
-      position = 0;//resets qoute position
+      position = 0;//reset qoute position
       correct = 0;//reset correct inputs
       quote.innerHTML= ""//clear qoute inner HTML
       populateText(theme);//call function to set next quote
@@ -91,8 +91,7 @@ function themeSelect(theme){
 function populateText(theme){
   let tracker = 0;
   let str = theme[arrayPosition];
-  // str.split("") will convert our string to characters array
-  // Then we can loop through them
+  // str.split("") will convert our string to characters array. THEN we can loop through them 
   str.split("").map(letter => {
       const span = document.createElement("span");
       span.innerText = letter;
@@ -114,7 +113,8 @@ function inputFilter(event){
       correct = correct + 1;
       accuracyHTML.innerHTML = "Accuracy: " + Math.trunc(correct/position*100) + "%";
       document.getElementById("character" + `${position-1}`).classList.remove("border", "blink");
-  } else if (test !== "Shift" && test !== document.getElementById("character" + `${position}`).innerHTML){//not shift and not equal to innerHTML
+  } 
+  else if (test !== "Shift" && test !== document.getElementById("character" + `${position}`).innerHTML){//not shift and not equal to innerHTML
       document.getElementById("character" + `${position}`).classList.add("incorrect"); 
       position = position + 1;
       accuracyHTML.innerHTML = "Accuracy: " + Math.trunc(correct/position*100) + "%";
@@ -124,7 +124,8 @@ function inputFilter(event){
   //update border position if position is not equal to length and qoute.innerHTML not ""
   if (position !== length && quote.innerHTML !== ""){
     document.getElementById("character" + `${position}`).classList.add("border", "blink");
-  } else if (position === length && qouteParent.innerHTML !== ""){//if at last character of qoute
+  } 
+  else if (position === length && qouteParent.innerHTML !== ""){//if at last character of qoute
     document.getElementById("continue").classList.remove("hidden");//unhide press any key to continue 
   }
 }
@@ -143,6 +144,7 @@ function restultsHTML(){
   //make results div 
   let parent = document.createElement('div');//parent div 
   parent.id = 'results'; //parent id="results"
+  parent.className = 'j-center';//parent class="j-center"
   let h1 = document.createElement('h1');//create h1 tag 
   h1.id = 'user-score'; 
   h1.className = 'title is-1 has-text-white';  
